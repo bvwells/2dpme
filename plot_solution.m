5e8131425828f17a79f35bd017869135129e4153
Ben Wells <bwells@slb.com>
2017-10-04 21:11:01 +0100

    Add titles to plots

--- plot_solution.m
+++ plot_solution.m
@@ -3,13 +3,18 @@ clear all;
 load 'variables.m';
 load 'cells.m';
 
+t_init = variables(4);
+output_t = variables(5);
 numberReports = variables(6);
+dt = (output_t-t_init)/numberReports;
 
 for report = 0:numberReports 
    
    filename = strcat('solution', sprintf('%03d',[report]), '.m');
 
-   u = load(filename);   
+   u = load(filename);
+   
+   time = t_init + dt*report
    
    figure(report+1);
    trimesh(cells,u(:,1),u(:,2),u(:,3));
@@ -18,5 +23,6 @@ for report = 0:numberReports
    xlabel('x');
    ylabel('y');
    zlabel('u');
+   title(strcat('Porous Medium Equation Solution at t=', num2str(time)))
 
  end
